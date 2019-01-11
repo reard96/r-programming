9cd34a296a27ccc1cf9514e0177c704eb4b88899
Dan Reardon <reardon.danielj@gmail.com>
2019-01-11 14:57:53 -0500

    q1 complete

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,8 +1,21 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  path <- paste("./data/", directory, sep="")
-  file_list <- list.files(path=path, pattern="*.csv")
+
+  vals <- vector()
 
   for (i in id) {
-    data <- read.csv()
+    # Grab data from relevant csv
+    file_name <- sprintf('%03d.csv', i)
+    file_path <- paste('./data', directory, file_name, sep='/')
+    data <- read.csv(file_path)
+
+    # Grab correct column
+    d <- data[,pollutant]
+
+    # Data cleaning
+    d <- d[!is.na(d)]
+
+    # Append to vector
+    vals <- c(vals, d)
   }
+  mean(vals)
 }
