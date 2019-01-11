0994ee3bcb0a33afb564c835844e8dea253ce8bf
Dan Reardon <reardon.danielj@gmail.com>
2019-01-11 14:42:53 -0500

    first function started

---
+++ pollutantmean.R
@@ -0,0 +1,8 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  path <- paste("./data/", directory, sep="")
+  file_list <- list.files(path=path, pattern="*.csv")
+
+  for (i in id) {
+    data <- read.csv()
+  }
+}
