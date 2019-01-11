08237cf58e89346d7ef6bd8b07e43440de6276ad
Dan Reardon <reardon.danielj@gmail.com>
2019-01-11 15:29:04 -0500

    q2

--- complete.R
+++ complete.R
@@ -1,6 +1,8 @@
 complete <- function(directory, id = 1:332) {
 
-  vals <- vector()
+  df <- data.frame(matrix(ncol = 2, nrow = 0))
+  x <- c('id', 'nobs')
+  colnames(df) <- x
 
   for (i in id) {
     # Grab data from relevant csv
@@ -9,10 +11,10 @@ complete <- function(directory, id = 1:332) {
     data <- read.csv(file_path)
 
     # Data cleaning
-    data <- data[!is.na(data)]
+    data <- data[complete.cases(data), ]
+    num_complete <- nrow(data)
 
-    # Append to vector
-    vals <- c(vals, data)
+    df[nrow(df) + 1, ] = list(i, num_complete)
   }
-  length(vals)
+  df
 }
